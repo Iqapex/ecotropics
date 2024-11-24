805c6e2e9d27c3860b000606f0b526920ac6c728
Sunny Kumar <sunnykumar49617@gmail.com>
2024-11-24 10:41:47 +0530

    Removed all existing code

--- Backend/routes/donationRoutes.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const { processDonation } = require('../controllers/donationController');
-
-// router.post('/pay', processDonation);
-
-// module.exports = router;
